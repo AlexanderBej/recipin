81756841c69f2b59bb6ff9bf86efd2fc5adaffdb
Alexandru Bej <alexbej07@gmail.com>
2025-11-11 19:29:00 +0100

    some changes

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,24 +1,7 @@
-import {
-  combineReducers,
-  configureStore,
-  createListenerMiddleware,
-  isAnyOf,
-} from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './auth-store/auth.slice';
 import recipesReducer from './recipes-store/recipes.slice';
-
-// const lm = createListenerMiddleware();
-
-// lm.startListening({
-//   matcher: isAnyOf(userSignedOut),
-//   effect: async (_, api) => {
-//     api.dispatch(cleanupListeners());
-//     api.dispatch(resetHistory());
-//     api.dispatch(resetTxnFilters());
-//     localStorage.removeItem('month');
-//   },
-// });
 
 const rootReducer = combineReducers({
   auth: authReducer,
